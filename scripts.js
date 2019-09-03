6f65212d1f397e711e6f51e031e762e925ef6757
Nanna <5608562@rts-365.dk>
2019-09-03 21:08:35 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
 
+let newDiv = document.createElement("div");
+let textNode = newDiv.createTextNode("new div");
+newDiv.appendChild(textNode);
+let bodyElement = document.querySelector("body");
+bodyElement.appendChild(bodyElement)
